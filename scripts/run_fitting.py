b25eb70cdac005fd58c7ac5f60b5b370434821c5
Ridhima Nunhokee <ridhima@192-168-1-102.tpgi.com.au>
2023-10-11 12:41:30 +0800

    modified running of the scripts with more options

--- scripts/run_fitting.py
+++ scripts/run_fitting.py
@@ -1,25 +1,41 @@
 from argparse import ArgumentParser
-from pathlib import Path
-from polyFit.fitting import Fit
+from fitcal.gain_fitting import HyperSolnFits
 import numpy as np
 
-parser = ArgumentParser(description="Polynomial Fitting of the calibration solutions")
+parser = ArgumentParser(
+    description="Polynomial Fitting of the calibration solutions")
 parser.add_argument('soln', type=str, help='Hyperdrive fits file')
-parser.add_argument('-s', '--start', dest='start', default=3, type=int, help='Starting polynomial order to fit for. Default is 3.')
-parser.add_argument('-n', '--norder', dest='norder', default=7, type=int, help='number of order to iterate over. Defualt is 7.')
-parser.add_argument('-o', '--outfile', dest='outfile', default=None, help='Output file containting the fitted data')
+parser.add_argument('-a', '--amplitude', action='store_true',
+                    help='If set, will smooth the amplitudde of the calibration solutions.')
+parser.add_argument('-p', '--phase', action='store_true',
+                    help='If set, will fit the phase of the calibration solutions.')
+parser.add_argument('-n', '--order', dest='order', default=3,
+                    type=int, help='Polynomial order to fit for for the amplitudes.')
+parser.add_argument('-w', '--window', dest='window', default=99, type=int,
+                    help='Window size for the Savitzky-Golay Filter for amplitude smoothing.')
+parser.add_argument('-o', '--outfile', dest='outfile', default=None,
+                    help='Output file containting the fitted data.')
 
 args = parser.parse_args()
-m = Fit(args.soln)
-orders = np.arange(args.start, args.start + args.norder)
-errors = []
-for order in orders:
-    m.fit_data(order)
-    mse = m.mean_square_error()
-    errors.append(mse)
-    print ('Polynomial Order {} : MSE : {}'.format(order, mse))
-# finding order with the least error
-ind = errors.index(min(errors))
-print ('Choosing polynomial order {} with MSE {}'.format(orders[ind], errors[ind]))
-m.fit_data(orders[ind])
-m.write_to(args.outfile, ORDER=orders[ind], MSE=errors[ind])
+
+hypersoln = HyperSolnFits(args.soln)
+if args.amplitude and args.phase:
+    hypersoln.get_fitted_solutions(
+        order=args.order, window=args.window, weights=None, fit_amplitude=True, fit_phase=True)
+
+elif args.amplitude and not args.phase:
+    hypersoln.get_fitted_solutions(
+        order=args.order, window=args.window, weights=None, fit_amplitude=True, fit_phase=False)
+
+elif not args.amplitude and args.phase:
+    hypersoln.get_fitted_solutions(
+        order=args.order, window=args.window, weights=None, fit_amplitude=False, fit_phase=True)
+
+else:
+    hypersoln.get_fitted_solutions(
+        order=args.order, window=args.window, weights=None, fit_amplitude=False, fit_phase=False)
+
+# plotting the fitted solutions
+hypersoln.plot_solutions(args.outfile)
+# saving the solutionss
+hypersoln.write_to(args.outfile)
